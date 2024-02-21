65f0d97ae429045eb40a44498ff28e13c78a241c
Luke Henderson <personal@lukeh.us>
2024-02-21 02:11:25 -0800

    made buttons functional

--- website/epmain.py
+++ website/epmain.py
@@ -11,3 +11,65 @@ def home():
             title="Home"
         )
     )
+
+@epmain.route("/yt")
+@epmain.route("/youtube")
+def youtube():
+    return(
+        redirect(
+            "https://www.youtube.com/@AlgoraKiko"
+        )
+    )
+
+@epmain.route("/t")
+@epmain.route("/x")
+@epmain.route("/twt")
+@epmain.route("/twitter")
+def twitter():
+    return(
+        redirect(
+            "https://twitter.com/AlgoraKiko"
+        )
+    )
+
+@epmain.route("/ttv")
+@epmain.route("/twitch")
+def twitch():
+    return(
+        redirect(
+            "https://twitch.tv/AlgoraKiko"
+        )
+    )
+
+@epmain.route("/discord")
+def discord():
+    return(
+        redirect(
+            "https://discord.com/invite/cFVFG26"
+        )
+    )
+
+@epmain.route("/insta")
+@epmain.route("/instagram")
+def instagram():
+    return(
+        redirect(
+            "https://www.instagram.com/algorakiko/"
+        )
+    )
+
+@epmain.route("/tiktok")
+def tiktok():
+    return(
+        redirect(
+            "https://www.tiktok.com/@algorakiko"
+        )
+    )
+
+@epmain.route("/spotify")
+def spotify():
+    return(
+        redirect(
+            "https://open.spotify.com/user/jailenetalampas"
+        )
+    )
